307dbaec4de52e73024da968efc021fb5f04882b
shanlihou <shanlihou@gmail.com>
2022-06-04 16:29:25 +0800

    first blood

---
+++ dnspod.py
@@ -0,0 +1,55 @@
+from tkinter.dnd import DndHandler
+import requests
+import json
+import conf
+import logging
+
+URI_BASE = 'https://dnsapi.cn'
+
+
+class DnspodHelper(object):
+    def get_record(self):
+        url = '{}/{}'.format(URI_BASE, 'Record.List')
+        data = {
+            'login_token': conf.LOGIN_TOKEN,
+            'domain': conf.DOMAIN,
+        }
+
+        ret = requests.post(url, data=data)
+        return ret.text
+
+    def save(self, filename, content):
+        with open(filename, 'w') as fw:
+            fw.write(content)
+
+    def load(self, filename):
+        with open(filename) as fr:
+            return fr.read()
+
+    def parse_record(self, content):
+        ret_data = json.loads(content)
+
+        for rec in ret_data['records']:
+            print(rec)
+
+    def update_ddns(self, ip_str):
+        url = '{}/{}'.format(URI_BASE, 'Record.Ddns')
+        data = {
+            'login_token': conf.LOGIN_TOKEN,
+            'domain': conf.DOMAIN,
+            'record_id': conf.RECORD_ID,
+            'record_line': '默认',
+            'record_line_id': 0,
+            'value': ip_str
+        }
+        logging.info('update ddns:{}'.format(data))
+        ret = requests.post(url, data=data)
+        logging.info('result:{}'.format(ret.text))
+
+
+if __name__ == '__main__':
+    dh = DnspodHelper()
+    #ret = dh.get_record()
+    #dh.save('.record', ret)
+    ret = dh.load('.record')
+    dh.parse_record(ret)
---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import mail
+import time
+import os
+import logging
+import dnspod
+import conf
+
+
+def send_mail(ip_str):
+    _mail = mail.get_default_user_mail('.mail_user_info.json')
+    _mail.send_mail(conf.RECEIVER_EMAIL_ADDRESS, 'new ip:{}'.format(ip_str), ip_str)
+
+
+def get_ip():
+    url = 'https://v4.myip.la'
+    ret = requests.get(url)
+    return ret.text
+
+
+def load_last_ip():
+    if not os.path.exists('.last_ip'):
+        return ''
+
+    with open('.last_ip') as fr:
+        return fr.read()
+
+
+def save_ip(ip_str):
+    with open('.last_ip', 'w') as fw:
+        fw.write(ip_str)
+
+
+def run():
+    while 1:
+        try:
+            _last_ip = load_last_ip()
+            _ip = get_ip().strip()
+            print(_ip, _last_ip)
+            if _ip != _last_ip:
+                send_mail(_ip)
+
+            dh = dnspod.DnspodHelper()
+            dh.update_ddns(_ip)
+            save_ip(_ip)
+
+        except Exception as e:
+            print(e)
+        time.sleep(10 * 60)
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='ddns.log', level = getattr(logging, 'DEBUG'), format='%(levelname)s %(asctime)s %(message)s')
+    main()
+
